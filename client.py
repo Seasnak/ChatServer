5a7951b7a441e4dcc8f3ea08811b43dea9e6c977
Sasank Madineni <sasank1020@gmail.com>
2020-06-07 16:41:28 -0700

    Added some code for client.py and server.py

--- client.py
+++ client.py
@@ -3,5 +3,29 @@ import sys
 import socket
 import time
 
+def connect_to_server(ip, sock):
+    return 0
+
 if __name__ == "__main__":
-    print("Client Script Started!")
+    print("Client Script Initialized!")
+
+    username = input("Enter Username: ")
+    server_ip = "127.0.0.1" #input("Enter Server IP: ")
+    port = 8096 #int(input("Enter Port: "))
+    client_ip = socket.AF_INET
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    client_socket.connect((server_ip, port))
+    print(f"Connected to Server at IP {server_ip}.")
+    client_socket.listen()
+    conn, addr = client_socket.accept()
+    message = ""
+    while True:
+        data = conn.recv(4096)
+        if not data: break
+        message += data
+    print(message)
+    conn.close()
+    print("Disconnected from Server")
--- server.py
+++ server.py
@@ -3,5 +3,42 @@ import sys
 import socket
 import time
 
+port = 5000
+
+# def receive_message(client_socket) :
+
+
 if __name__ == "__main__":
-    print("Server Script Started!")
+    print("Server Script Initialized!")
+
+    ip = "127.0.0.1" #input("Enter IP: ")
+    port = int(input("Enter Port Number: "))
+
+    # AF_INET = ipv4 address, SOCK_STREAM = TCP Connection
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # set socket to be able to take packets from the same ip (SO_REUSEADDR = 1)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #binding the ip to the port and listening
+    try:
+        server_socket.bind((ip, port))
+    except:
+        print(f"Error: Couldn't bind {ip} to {port}")
+        sys.exit()
+    server_socket.listen()
+
+    sockets_list =  [server_socket]
+    clients = {}
+    print(f"Listening for connections on {ip}:{port}")
+
+    while True:
+        conn, addr = server_socket.accept()
+        print(f"Got Connection from {addr}")
+        conn.send("Server Received Message")
+        message = ""
+        while True:
+            data = conn.recv(4096)
+            if not data: break
+            message += data
+        print(message)
+        conn.close()
+        print("Client Disconnected")
