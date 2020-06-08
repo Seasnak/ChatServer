ee54f7a189af79cb51bbed134e2a25803942120e
Sasank Madineni <sasank1020@gmail.com>
2020-06-07 18:44:53 -0700

    Fixed Client - Server Chat Model for 1 Connection

--- client.py
+++ client.py
@@ -9,23 +9,30 @@ def connect_to_server(ip, sock):
 if __name__ == "__main__":
     print("Client Script Initialized!")
 
-    username = input("Enter Username: ")
-    server_ip = "127.0.0.1" #input("Enter Server IP: ")
-    port = 8096 #int(input("Enter Port: "))
-    client_ip = socket.AF_INET
-
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    username = input("Enter Username: ")
+    server_ip = input("Enter Server IP: ")
+    port = 8096 #int(input("Enter Port: "))
+    hostname = socket.gethostname()
+    client_ip = socket.gethostbyname(hostname)
+
     client_socket.connect((server_ip, port))
     print(f"Connected to Server at IP {server_ip}.")
-    client_socket.listen()
-    conn, addr = client_socket.accept()
-    message = ""
+    client_socket.send(username.encode())
+    server_name = client_socket.recv(1024)
+    server_name = server_name.decode()
+    print(f"Joined Server: {server_name}")
+    print("Enter [exit] to exit.")
     while True:
-        data = conn.recv(4096)
-        if not data: break
-        message += data
-    print(message)
-    conn.close()
-    print("Disconnected from Server")
+        message = input(f"{username} > ")
+        if message == "[exit]":
+            client_socket.send(message.encode())
+            message = client_socket.recv(1024)
+            if message.decode() == "[OK]":
+                print(f"Exiting from {server_name}.")
+                break
+            else:
+                print("Error: Couldn't exit chat server, please try again.")
+        client_socket.send(message.encode())
--- server.py
+++ server.py
@@ -11,13 +11,16 @@ port = 5000
 if __name__ == "__main__":
     print("Server Script Initialized!")
 
-    ip = "127.0.0.1" #input("Enter IP: ")
-    port = int(input("Enter Port Number: "))
-
     # AF_INET = ipv4 address, SOCK_STREAM = TCP Connection
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # set socket to be able to take packets from the same ip (SO_REUSEADDR = 1)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    hostname = socket.gethostname () #input("Enter IP: ")
+    ip = socket.gethostbyname(hostname)
+    port = 8096 #int(input("Enter Port Number: "))
+    server_name = input("Enter Server Name: ")
+
     #binding the ip to the port and listening
     try:
         server_socket.bind((ip, port))
@@ -30,15 +33,21 @@ if __name__ == "__main__":
     clients = {}
     print(f"Listening for connections on {ip}:{port}")
 
+    conn, addr = server_socket.accept()
+    print(f"Got Connection from {addr}")
+
+    client_name = conn.recv(1024)
+    client_name = client_name.decode()
+
+    message = f"{server_name}"
+    conn.send(message.encode())
+
     while True:
-        conn, addr = server_socket.accept()
-        print(f"Got Connection from {addr}")
-        conn.send("Server Received Message")
-        message = ""
-        while True:
-            data = conn.recv(4096)
-            if not data: break
-            message += data
-        print(message)
-        conn.close()
-        print("Client Disconnected")
+        message = conn.recv(1024)
+        message = message.decode()
+        if message == "[exit]":
+            message = "[OK]"
+            conn.send(message.encode())
+            print(f"{addr} has disconnected.")
+            break
+        print(client_name, '>', message)
